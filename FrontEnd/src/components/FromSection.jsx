a19925d64a0dbacfca0638addbf1dc7f75ff8d4e
sanket <sanketsnayak89@gmail.com>
2025-06-03 01:21:20 +0530

    created side bar

--- FrontEnd/src/components/FromSection.jsx
+++ FrontEnd/src/components/FromSection.jsx
@@ -7,15 +7,19 @@ import Education from './FormSection/Education'
 import Skills from './FormSection/Skills'
 import TextColor from './TextColor'
 import Download from './FormSection/Download'
-import { Navigate, useParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 function FromSection() {
   const [formIndex,setFormIndex]=useState(1)
   const [enableNext,setEnableNext]=useState(false)
   const {id}=useParams()
+  const navigate=useNavigate()
   return (
     <div>
       <div className='flex justify-between'>
+        <div className='flex gap-3'>
+        <Button onClick={()=>navigate('/dashboard')}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg></Button>
         <TextColor/>
+        </div>
         <div className='flex gap-2'>
           {
             formIndex>1?(<Button onClick={()=>setFormIndex(formIndex-1)}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg></Button>):""
--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -2,14 +2,21 @@ import { UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import AddResume from '@/components/AddResume.jsx'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 function Dashboard() {
   const {user}=useUser()
 
   
   return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarTrigger />
     <div>
+      
       <AddResume/>
     </div>
+    </SidebarProvider>
   )
 }
 
