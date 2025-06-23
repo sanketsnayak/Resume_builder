42f8da36e3928d39c25cb435813ab4a6845a8550
sanket <sanketsnayak89@gmail.com>
2025-06-23 13:42:47 +0530

    changes for deployment

--- BackEnd/Routes/uploadLinkedIn.js
+++ BackEnd/Routes/uploadLinkedIn.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import multer from 'multer';
 import AdmZip from 'adm-zip';
-import fs from 'fs';
-import { parse } from 'csv-parse/sync'; // <-- Add this
+import { parse } from 'csv-parse/sync';
 
 const router1 = express.Router();
-const upload = multer({ dest: 'uploads/' }); 
+
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 router1.post('/uploadLinkedIn', upload.single('file'), (req, res) => {
   try {
-    const zip = new AdmZip(req.file.path);
     
-    
+    const zip = new AdmZip(req.file.buffer);
+
     const getCSV = (filename) => {
       const entry = zip.getEntries().find(e => e.entryName.endsWith(filename));
       if (!entry) return null;
@@ -20,14 +21,13 @@ router1.post('/uploadLinkedIn', upload.single('file'), (req, res) => {
     };
 
     const profile = getCSV("Profile.csv");
-    const learning=getCSV("Learning.csv")
-    const certifications=getCSV("Certifications.csv")
-    const education=getCSV("Education.csv")
-    const positions=getCSV("Positions.csv")
-    const skills=getCSV("Skills.csv")
-    const phoneNumber=getCSV("PhoneNumbers.csv")
-    const email=getCSV("Email Addresses.csv")
-    fs.unlinkSync(req.file.path); // cleanup
+    const learning = getCSV("Learning.csv");
+    const certifications = getCSV("Certifications.csv");
+    const education = getCSV("Education.csv");
+    const positions = getCSV("Positions.csv");
+    const skills = getCSV("Skills.csv");
+    const phoneNumber = getCSV("PhoneNumbers.csv");
+    const email = getCSV("Email Addresses.csv");
 
     res.json({
       success: true,
